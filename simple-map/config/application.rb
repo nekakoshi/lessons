1af234f5480131c81b5ee1c02e7ba72eb9e57aab
nekakoshi <harada@weatherlight.co.jp>
2020-12-06 15:02:40 +0900

    Add i18n

--- simple-map/config/application.rb
+++ simple-map/config/application.rb
@@ -11,5 +11,15 @@ module SimpleMap
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
+    #　以下の記述を追記する(設定必須)
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+    
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
   end
 end
